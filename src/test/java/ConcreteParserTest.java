35e6f2f26f4a634cabcdd5a2b66ad0ab9af62679
I.Nika@student.tudelft.nl <i.nika@student.tudelft.nl>
2020-04-25 18:16:22 +0200

    pmd fix and checkstyle. Changes to parser test.

--- src/test/java/ConcreteParserTest.java
+++ src/test/java/ConcreteParserTest.java
@@ -1,27 +1,13 @@
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
-
-import java.io.FileNotFoundException;
-import java.util.stream.Stream;
 
 public class ConcreteParserTest {
 
     @Test
-    public void parseTest(){
+    public void parseTest() {
         AbstractParserClass abstractParserClass = new ConcreteParser();
-
         Object[] stream = abstractParserClass.parse("src/test/resources/test.txt").toArray();
-        Object[] temp = new Object[1];
-        temp[0] = new Record(0, 2);
-
-        assert(temp.length == stream.length);
-        assert(temp.length == 1);
-        assert(temp[0].getClass() == Record.class);
-        assert(stream[0].getClass() == Record.class);
-        Record record1 = (Record) temp[0];
+        Record record1 = new Record(0, 2);
         Record record2 = (Record) stream[0];
-        assert(record1.toString().equals(record2.toString()));
-
+        assert (record1.toString().equals(record2.toString()));
     }
-
 }
--- src/test/java/RecordTest.java
+++ src/test/java/RecordTest.java
@@ -3,7 +3,7 @@ import org.junit.jupiter.api.Test;
 public class RecordTest {
 
     @Test
-    public void toStringTest(){
+    public void toStringTest() {
         Record record  = new Record(0, 1);
         String temp = "0 1";
         assert (temp.equals(record.toString()));
