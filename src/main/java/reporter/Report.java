3e54e6489d59372b941085b50cd8ad49c2d2b224
Gancho <G.Georgiev@student.tudelft.nl>
2020-04-28 05:07:21 -0700

    improved report
    improved report

--- src/main/java/reporter/Report.java
+++ src/main/java/reporter/Report.java
@@ -1,26 +1,33 @@
 package reporter;
 
 import java.io.FileWriter;
+import java.text.DecimalFormat;
 
 public class Report {
-    transient private double percentage;
+    private transient String policyType;
+    private transient double hitRatio;
 
     /**
      * Constructor for the report.
      *
-     * @param percentage The percentage of success of cache hits/miss.
+     * @param policyType The cache policy type.
+     * @param hitRatio The percentage of hit success.
      */
-    public Report(double percentage) {
-        this.percentage = percentage;
+    public Report(String policyType, double hitRatio) {
+        this.policyType = policyType;
+        this.hitRatio = hitRatio;
     }
 
     /**
-     * Creating the report.
+     * Creating the report in a csv format.
      */
     public void report() {
+        DecimalFormat df = new DecimalFormat("#.##");
+        String hitRatio = String.valueOf(df.format(this.hitRatio * 100)) + '%';
+        String missRatio = String.valueOf(df.format((1 - this.hitRatio) * 100)) + '%';
         try {
             FileWriter fw = new FileWriter("src/main/resources/reports/report.txt");
-            fw.write(String.valueOf(this.percentage));
+            fw.write(this.policyType + "," + hitRatio + "," + missRatio);
             fw.close();
         } catch (Exception e) {
             System.out.println();
