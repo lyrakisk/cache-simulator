a36eefcd0c268c4a56359b20cc7c8477435a280e
Kostas Lyrakis <k.lyrakis@student.tudelft.nl>
2020-05-04 18:47:10 +0300

    updated javadoc for AbstractParserClass, CambridgeTraceParser. Updated CambridgeTraceParser test to use the Record getters instead of the toString method.

--- src/main/java/parser/AbstractParserClass.java
+++ src/main/java/parser/AbstractParserClass.java
@@ -4,11 +4,17 @@ import java.util.stream.Stream;
 
 public abstract class AbstractParserClass {
     /**
-     * Parse a record.
+     * Parse a trace file. These files come in different formats
+     * (e.g. .txt, .csv, .trace etc...)
      * @param filename name of the file.
      * @return Stream of records.
      */
     public abstract Stream<Record> parse(String filename);
 
+    /**
+     * Parses one line of a trace file and converts it to a record.
+     * @param line line of a trace file.
+     * @return record.
+     */
     public abstract Record parseRecord(String line);
 }
