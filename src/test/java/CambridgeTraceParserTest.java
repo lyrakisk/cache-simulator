fa97cf87f9031ec0afa7b3d2637705ad2537bbc1
Kostas Lyrakis <k.lyrakis@student.tudelft.nl>
2020-05-06 21:46:08 +0300

    Added test to CambridgeTraceParser

--- src/test/java/CambridgeTraceParserTest.java
+++ src/test/java/CambridgeTraceParserTest.java
@@ -1,8 +1,16 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.junit.jupiter.api.Test;
 import parser.Record;
 import parser.snia.CambridgeTraceParser;
+
 
 public class CambridgeTraceParserTest {
 
@@ -16,5 +24,23 @@ public class CambridgeTraceParserTest {
         Record expected = new Record("9056014336", 2048);
         assertEquals(expected.getSize(), actual.getSize());
         assertEquals(expected.getId(), actual.getId());
+    }
+
+    @Test
+    public void whenExceptionThrown_thenAssertionSucceeds() {
+        final PrintStream originalErr = System.err;
+        final ByteArrayOutputStream err = new ByteArrayOutputStream();
+        System.setErr(new PrintStream(err));
+
+        Stream<Record> records =
+                (new CambridgeTraceParser()).parse("pita.gyros");
+        String expectedMessage = "ERROR: The file named pita.gyros was not found!\n";
+        List<Record> collectedRecords =
+                records.sequential().collect(Collectors.toList());
+
+        assertEquals(expectedMessage, err.toString());
+        assertTrue(collectedRecords.isEmpty());
+
+        System.setErr(originalErr);
     }
 }
