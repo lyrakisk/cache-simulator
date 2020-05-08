ca8f592b8dc830bd476bbfc209c9ac6e35817292
Kostas Lyrakis <k.lyrakis@student.tudelft.nl>
2020-05-08 12:37:33 +0300

    Moved ArcTraceParser to arc Package.

--- src/main/java/parser/arc/ArcTraceParser.java
+++ src/main/java/parser/arc/ArcTraceParser.java
@@ -1,4 +1,4 @@
-package parser.snia;
+package parser.arc;
 
 import java.io.BufferedReader;
 import java.io.FileInputStream;
--- src/test/java/ArcTraceParserTest.java
+++ src/test/java/ArcTraceParserTest.java
@@ -2,7 +2,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 import parser.Record;
-import parser.snia.ArcTraceParser;
+import parser.arc.ArcTraceParser;
 
 
 
