6ecc8edf53da368598f5979bd7494d559db926cc
I.Nika@student.tudelft.nl <i.nika@student.tudelft.nl>
2020-05-22 15:32:55 +0200

    Add trace file found in UMass repositories.

---
+++ src/main/java/parser/umass/UmassTraceParser.java
@@ -0,0 +1,13 @@
+package parser.umass;
+
+/**
+ * Traces taken  from and described here:
+ * http://traces.cs.umass.edu/index.php/Storage/Storage?fbclid=IwAR2XFXBKz2AKUgV8UUPgo2fBqmJ9sF6gwc1K_l9wo9EhdkTWfxZZIrdmjK4
+ *
+ */
+
+public class UmassTraceParser {
+
+
+
+}
