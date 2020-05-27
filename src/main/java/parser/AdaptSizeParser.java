ad416607a8939cd7297c99424bccef4cc3ff7867
Ioanna <I.Nika@student.tudelft.nl>
2020-05-27 20:47:52 +0200

    Add trace file

---
+++ src/main/java/parser/AdaptSizeParser.java
@@ -0,0 +1,23 @@
+package parser;
+
+
+/**
+ * Trace parser for the files taken from:
+ * https://github.com/dasebe/webcachesim#how-to-get-traces
+ *
+ * time	id	size
+ * 1	1	120
+ * 2	2	64
+ * 3	1	120
+ * 4	3	14
+ *
+ */
+public class AdaptSizeParser extends AbstractParserClass {
+
+
+
+    @Override
+    public Record parseRecord(String line) {
+        return null;
+    }
+}
